016d048fc32857f8912df8ba44a165234413c212
mmarc <mmarcell2003@gmail.com>
2025-02-17 23:43:29 +0100

    Added in the ability to get and post data; accurae all the fleet data with get

--- api.py
+++ api.py
@@ -1,55 +1,75 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_restful import reqparse, Resource, marshal_with, fields, Api
 
 app = Flask(__name__)
 
-# For practice only
-books_dict = [
-    {
-        "id": 1,
-        "title": "The Catcher in the Rye",
-        "author": "J.D. Salinger",
-        "genre": "Fiction",
-    },
-    {
-        "id": 2,
-        "title": "1984",
-        "author": "George Orwell",
-        "genre": "Dystopian",
-    },
-    {
-        "id": 3,
-        "title": "To Kill a Mockingbird",
-        "author": "Harper Lee",
-        "genre": "Fiction",
-    }
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vehicle_fleet.db" # Tells SQLAlchemy what database to connect to
+db = SQLAlchemy(app=app)
 
-@app.route('/books' , methods=['GET', 'POST'])
-def books():
-    if request.method == 'GET':
-        if len(books_dict) > 0:
-            return jsonify(books_dict)
+api = Api(app=app)
+
+class Vehicle(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    fuel_type: Mapped[str] = mapped_column(nullable=False) # Either fuel or hybrid
+    distance: Mapped[int] = mapped_column(nullable=False) # distance in km for how far a car can go
+    seats: Mapped[int] = mapped_column(nullable=False) # number of seats available for passangers
+    license_plate_number: Mapped[str] = mapped_column(nullable=False, unique=True)
+    car_brand: Mapped[str] = mapped_column(nullable=False)
+    driver_name: Mapped[str] = mapped_column(nullable=False)
+
+# Argument parsing
+
+vehicle_args = reqparse.RequestParser()
+vehicle_args.add_argument('fuel_type', type=str, help='Type of fuel or mode a car uses.')
+vehicle_args.add_argument('distance', type=int, help='How far a car can go with one full tank.')
+vehicle_args.add_argument('seats', type=int, help='Number of available seats in the vehicle.')
+vehicle_args.add_argument('license_plate_number', type=str, help='License plate number of the car.')
+vehicle_args.add_argument('car_brand', type=str, help='Brand of the car.')
+vehicle_args.add_argument('driver_name', type=str, help='The name of the driver.')
+
+fleet_vehicles = {
+    'id': fields.Integer,
+    'fuel_type': fields.String,
+    'distance': fields.Integer,
+    'seats': fields.Integer,
+    'license_plate_number': fields.String,
+    'car_brand': fields.String,
+    'driver_name': fields.String
+}
+
+
+# Routing using the flask_restful package
+
+# Get all information on the fleet
+class GetAllFleet(Resource):
+    @marshal_with(fleet_vehicles)
+    def get(self):
+        whole_fleet = Vehicle.query.all()
+        return whole_fleet
+    
+    @marshal_with(fleet_vehicles)
+    def post(self):
+        args = vehicle_args.parse_args()
+        new_entry = Vehicle(fuel_type = args['fuel_type'], 
+                            distance = args['distance'], 
+                            seats = args['seats'], 
+                            license_plate_number = args['license_plate_number'],
+                            car_brand = args['car_brand'],
+                            driver_name = args['driver_name'])
         
-        else:
-            'Nothing found', 404
+        db.session.add(new_entry)
+        db.session.commit()
+        fleet = Vehicle.query.all()
+        return fleet, 201
 
-    if request.method == 'POST':
-        new_title = request.form['title']
-        new_author = request.form['author']
-        new_genre = request.form['genre']
-        iD = books_dict[-1]['id']+1
+api.add_resource(GetAllFleet, '/all/fleet')
 
-        new_object = {
-        "id": iD,
-        "title": new_title,
-        "author": new_author,
-        "genre": new_genre,
-        }
 
-        books_dict.append(new_object)
-        return jsonify(books_dict), 201
+@app.route('/')
+def name():
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True)
